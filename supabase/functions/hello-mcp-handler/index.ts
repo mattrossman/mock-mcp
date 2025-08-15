2e60624c62c4beedea4469386a8a1f310382559f
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2025-08-15 10:03:07 -0400

    Add mcp-handler function

---
+++ supabase/functions/hello-mcp-handler/index.ts
@@ -0,0 +1,36 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+
+import { createMcpHandler } from "mcp-handler";
+import { z } from "zod";
+
+const functionName = "hello-mcp-handler";
+
+const handler = createMcpHandler(
+  (server) => {
+    server.tool(
+      "roll_dice",
+      "Rolls an N-sided die",
+      {
+        sides: z.number().int().min(2),
+      },
+      ({ sides }) => {
+        const value = 1 + Math.floor(Math.random() * sides);
+        return {
+          content: [{ type: "text", text: `🎲 You rolled a ${value}!` }],
+        };
+      },
+    );
+  },
+  {},
+  {
+    basePath: `/${functionName}`,
+    disableSse: true,
+  },
+);
+
+Deno.serve(handler);
